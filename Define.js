c6feef8d598754e569880e17d25f81f0f8787a94
perflex <42873348+purrflex@users.noreply.github.com>
2022-11-28 11:37:28 +0700

    Create Define.js

---
+++ Define.js
@@ -0,0 +1,5 @@
+const canvas1 = document.getElementById('canvas');
+canvas1.width = window.innerWidth, canvas1.height = window.innerHeight;
+
+var Tool = new Construct('2d', canvas1.width / 2, canvas1.height / 2);
+var report = document.getElementById('Info');
