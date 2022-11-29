8105ab5c3a81ff8d57dc29e3c39f60ce022b12c1
perflex <42873348+purrflex@users.noreply.github.com>
2022-11-29 21:05:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,18 +11,12 @@ canvas.addEventListener('mousemove', function(event) {
   let endPoint = Math.sqrt(rX ** 2 + rY ** 2);
 
   Tool.Walls.forEach(function(i) {
-    var { x, y, iT } = Interpl(
-      { x: ray.x, y: ray.y },
-      { x: cx, y: cy },
-      { x: i[0][0], y: i[0][1]},
-      { x: i[1][0], y: i[1][1]}
-    )
-    
+    var { x, y, iT } = Interpl({ x: ray.x, y: ray.y }, { x: cx, y: cy },
+                               { x: i[0][0], y: i[0][1]}, { x: i[1][0], y: i[1][1]})
     if(iT) {
       Tool.Dot(x, y, 'red', 5)
       endPoint = Math.sqrt((x - ray.x) ** 2 + (y - ray.y) ** 2);
     }
-    console.log(angle)
     Tool.Ray(ray.x, ray.y, angle, endPoint);
   });
 });
