b670b43eef21461817bbff7db7a8ef5813adce09
perflex <42873348+purrflex@users.noreply.github.com>
2022-12-09 08:24:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
-canvas.addEventListener('mousemove', function(event) {
-  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-  Tool.Dot(ray.x, ray.y, 'black', 5)
-  Tool.Wall();
+class Main {
+  constructor(name, cb) {
+    this.name = name;
+    this.cb = cb;
+  }
   
-  let cx = event.clientX;
-  let cy = event.clientY;
-  let rX = cx - ray.x;
-  let rY = cy - ray.y;
-  let angle = Math.atan2(rY, rX)
-  let endPoint = Math.sqrt(rX ** 2 + rY ** 2);
+  Create() {
+    this.app = new App(this.name, this.cb)
+    this.app.runLoop();
+  }
+  
+  async Update(msg) {
+    msg = await msg.data.text();
+    msg = JSON.parse(msg)
+    
+    console.log(msg)
+    return;
+  }
 
-  Tool.Walls.forEach(function(i) {
-    var { x, y, iT } = Interpl({ x: ray.x, y: ray.y }, { x: cx, y: cy },
-                               { x: i[0][0], y: i[0][1]}, { x: i[1][0], y: i[1][1]})
-    if(iT) {
-      Tool.Dot(x, y, 'red', 5)
-      endPoint = Math.sqrt((x - ray.x) ** 2 + (y - ray.y) ** 2);
-    }
-    Tool.Ray(ray.x, ray.y, angle, endPoint);
-  });
+  Close() {
+    this.app.stopLoop();
+  }
+}
+
+async function RequestHandler(id) {
+  const response = await fetch(`https://Raycast-Map-Server.bennguyen2.repl.co/?map=${id}`);
+  return response.json();
+} 
+
+const params = new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, prop) => searchParams.get(prop),
 });
+
+(async function() {
+  let promised = await RequestHandler(params.map) || [];
+  if('error' in promised || !promised) return console.log(promised);
+  promised.seed.forEach((i) => Tool.Set(...i));
+  const main = new Main('Doc', Calculate)
+  main.Create();
+})()
